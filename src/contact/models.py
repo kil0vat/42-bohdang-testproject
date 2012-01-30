2493628564112aaba17248fb5372f37827b7f621
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-01-30 21:30:39 +0200

    ticket:1 added Contact model

--- src/contact/models.py
+++ src/contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birth_data = models.DateField()
+    bio = models.TextField()
+    email = models.EmailField()
+    jabber = models.EmailField()
+    skype = models.CharField(max_length=30)
+    other_contacts = models.TextField()
