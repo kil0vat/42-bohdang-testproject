149eec89f9f687dc6b2010a25f747e740a9a925d
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-02-01 15:50:01 +0200

    ticket:3 started requests_log app with a simple test

---
+++ src/requests_log/tests.py
@@ -0,0 +1,17 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+
+
+class SimpleTest(TestCase):
+    def test_requests_page_exists(self):
+        """
+        Tests that requests page exists.
+        """
+        response = self.client.get('/requests')
+        self.assertEqual(response.status_code, 200)
