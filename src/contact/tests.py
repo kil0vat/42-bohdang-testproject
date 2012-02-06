afcbcfb1a8c4603f370c6953bab27b33f996476f
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-02-07 00:36:17 +0200

    ticket:5 tests for updating form

--- src/contact/tests.py
+++ src/contact/tests.py
@@ -70,7 +70,7 @@ class EditContactTestCaseNonAuth(TestCase):
         """
         response = self.client.get('/edit/')
         self.assertEqual(response.status_code, 302)
-
+    
     def test_edit_page_redirects(self):
         """
         Test that page /edit/ redirects to /accounts/login/?next=/edit/.
@@ -86,7 +86,7 @@ class EditContactTestCaseNonAuth(TestCase):
         response = self.client.get('/edit/', follow=True)
         self.assertEqual(response.status_code, 200)
 
-class EditContactTestCase(TestCase):
+class EditContactTestCaseAuth(TestCase):
     def setUp(self):
         self.user = User.objects.create_user('test', 'dudarev+test@gmail.com', 'test')
         self.client.login(username='test', password='test')
@@ -116,3 +116,47 @@ class EditContactTestCase(TestCase):
         """
         contact = Contact.objects.get(pk=1)
         self.assertTrue(contact.skype in self.response.content)
+
+
+class EditContactTestCasePost(TestCase):
+    def setUp(self):
+        """
+        Set up login.
+        """
+        self.user = User.objects.create_user('test', 'dudarev+test@gmail.com', 'test')
+        self.client.login(username='test', password='test')
+        self.good_data = {
+                "bio": "Artem was born in Donetsk", 
+                "first_name": "Artem",
+                "last_name": "Dudarev", 
+                "other_contacts": "http://twitter.com/dudarev", 
+                "skype": "artem_dudarev", 
+                "birth_date": "1978-08-07", 
+                "jabber": "dudarev@gmail.com", 
+                "email": "dudarev@gmail.com"
+                }
+
+    def test_send_no_data(self):
+        """
+        Test when no data is sent.
+        """
+        response = self.client.post(reverse('edit_contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['form']['first_name'].errors, [u'This field is required.'])
+
+    def test_send_good_data_updates(self):
+        """
+        Test that when good data is sent contact is updated.
+        """
+        test_name = "Another Name"
+        self.good_data['first_name'] = test_name
+        self.client.post(reverse('edit_contact'), self.good_data)
+        contact = Contact.objects.get(pk=1)
+        self.assertEqual(contact.first_name, test_name)
+
+    def test_send_good_data_redirects(self):
+        """
+        Test that when good data is sent one is redirected.
+        """
+        response = self.client.post(reverse('edit_contact'), self.good_data, follow=True)
+        self.assertEqual(response.status_code, 200)
