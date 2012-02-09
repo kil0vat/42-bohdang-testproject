40bc5ef2b09fd8323ed44ef93e6e4a482d662449
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-02-09 23:04:33 +0200

    ticket:5 redirect accounts/profile to home page

--- src/contact/urls.py
+++ src/contact/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('contact.views',
     url(r'^$', 'index', name='index'),
+    url(r'^accounts/profile/$', redirect_to, {'url': '/'}),
     url(r'^edit/$', 'edit_contact', name='edit_contact'),
 )
