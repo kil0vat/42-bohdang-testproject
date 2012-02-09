385829cb422ef13b78f54071096184873f389823
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-02-09 12:43:40 +0200

    ticket:5 setting to handle media

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 from django.contrib.auth.views import login, logout
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -20,3 +21,14 @@ urlpatterns = patterns('',
         # Uncomment the next line to enable the admin:
         # url(r'^admin/', include(admin.site.urls)),
         )
+
+
+# ... the rest of your URLconf goes here ...
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+            url(r'^media/(?P<path>.*)$', 'django.views.static.serve', 
+                {
+                'document_root': settings.MEDIA_ROOT,
+                }),
+            )
