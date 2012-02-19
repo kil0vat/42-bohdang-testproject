b8fb94193d6729c03f4ee3dcd9a252cec2264802
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-02-19 16:23:57 +0200

    ticket:6 ajax form submission

--- src/contact/forms.py
+++ src/contact/forms.py
@@ -1,9 +1,10 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, IntegerField, HiddenInput
 
 from models import Contact
 from widgets import CalendarAJAXInput
 
 class ContactForm(ModelForm):
+    is_ajax_request = IntegerField(initial=0, widget=HiddenInput)
     class Meta:
         model = Contact
         widgets = {
--- src/contact/static/js/contact.js
+++ src/contact/static/js/contact.js
@@ -3,3 +3,30 @@ $(document).ready(function(){
         date_format: "YYYY-mm-dd"
     });
 }); 
+
+$(document).ready(function() { 
+    var options = { 
+        beforeSubmit: showRequest,
+        success: showResponse,
+        dataType: 'json'
+    };
+    $('#contact-form').ajaxForm(options); 
+
+}); 
+
+// pre-submit callback 
+function showRequest(formData, jqForm, options) { 
+    // set is_ajax_request field value to 1
+    $("input[name='is_ajax_request']").val(1);
+} 
+
+// post-submit callback 
+function showResponse(responseJSON, statusText, xhr, $form)  { 
+    if(responseJSON['success']){
+        $('#response-message-success').html(responseJSON['html']);
+        $('#response-modal-success').modal();
+    } else {
+        $('#response-message-error').html(responseJSON['html']);
+        $('#response-modal-error').modal();
+    };
+} 
