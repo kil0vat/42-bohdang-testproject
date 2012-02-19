f89047ed74a2b192227a2bcb73ea3cc2114092fc
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-02-19 17:03:56 +0200

    ticket:6 message about sending to server

--- src/contact/static/js/contact.js
+++ src/contact/static/js/contact.js
@@ -18,15 +18,21 @@ $(document).ready(function() {
 function showRequest(formData, jqForm, options) { 
     // set is_ajax_request field value to 1
     $("input[name='is_ajax_request']").val(1);
+    options = {
+        backdrop: 'static'
+    }
+    $('#message-submitting-data').modal(options);
 } 
 
 // post-submit callback 
 function showResponse(responseJSON, statusText, xhr, $form)  { 
     if(responseJSON['success']){
+        $('#message-submitting-data').modal('hide');
         $('#response-message-success').html(responseJSON['html']);
-        $('#response-modal-success').modal();
+        $('#response-modal-success').modal('show');
     } else {
+        $('#message-submitting-data').modal('hide');
         $('#response-message-error').html(responseJSON['html']);
-        $('#response-modal-error').modal();
+        $('#response-modal-error').modal('show');
     };
 } 
