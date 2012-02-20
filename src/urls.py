5f675ed41d8c179d1afff027d4258d768846fcf4
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-02-21 00:23:27 +0200

    ticket:8 link to admin

--- src/urls.py
+++ src/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth.views import login, logout
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
         ('', include('contact.urls')),
@@ -19,7 +19,7 @@ urlpatterns = patterns('',
         # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
         # Uncomment the next line to enable the admin:
-        # url(r'^admin/', include(admin.site.urls)),
+        url(r'^admin/', include(admin.site.urls)),
         )
 
 
