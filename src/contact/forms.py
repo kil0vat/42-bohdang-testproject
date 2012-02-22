3df96a874f975ea115617164f8a294226fe7fd24
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-02-22 19:14:42 +0200

    ticket:7 two forms are used with form.as_p

--- src/contact/forms.py
+++ src/contact/forms.py
@@ -1,12 +1,18 @@
 from django.forms import ModelForm, IntegerField, HiddenInput
 
-from models import Contact
+from models import Person, Contact
 from widgets import CalendarAJAXInput
 
-class ContactForm(ModelForm):
+class PersonForm(ModelForm):
     is_ajax_request = IntegerField(initial=0, widget=HiddenInput)
     class Meta:
-        model = Contact
+        model = Person
         widgets = {
                 'birth_date': CalendarAJAXInput(),
                 }
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        exclude = ('person',)
+
--- src/contact/views.py
+++ src/contact/views.py
@@ -7,8 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 
 from models import Contact
-from forms import ContactForm
-
+from forms import ContactForm, PersonForm
 
 def index(request):
     contact = get_object_or_404(Contact, pk=1)
@@ -24,36 +23,48 @@ def index(request):
 def edit_contact(request):
     contact = get_object_or_404(Contact, pk=1)
     if request.method == 'POST': 
-        form = ContactForm(
+        form_person = PersonForm(
                 request.POST, 
                 request.FILES,
+                prefix='person',
+                instance=contact.person
+                )
+        form_contact = ContactForm(
+                request.POST, 
+                request.FILES,
+                prefix='contact',
                 instance=contact
                 )
-        is_ajax_request = form.data.get('is_ajax_request',0)
-        if form.is_valid(): 
+        is_ajax_request = form_person.data.get('person-is_ajax_request',0)
+        if form_person.is_valid() and form_contact.is_valid(): 
             # Process the data in form.cleaned_data
-            form.save()
+            form_person.save()
+            form_contact.save()
             response = json.dumps({
                 'success': True,
                 'html': 'Data is succesfully updated.'
                 })
         else:
-            html = form.errors.as_ul()
+            html = form_person.errors.as_ul() + form_contact.errors.as_ul()
             response = json.dumps({'success': False, 'html': html})
 
         if is_ajax_request:
             return HttpResponse(response, content_type="application/javascript")
         else:
-            if form.is_valid():
+            if form_person.is_valid() and form_contact.is_valid(): 
                 return HttpResponseRedirect(reverse('index')) # Redirect after POST
     else:
-        form = ContactForm(
-                instance=contact
+        form_contact = ContactForm(
+                prefix="contact", instance=contact
+                )
+        form_person = PersonForm(
+                prefix="person", instance=contact.person
                 )
 
     return render_to_response('edit_contact.html', {
         'contact': contact,
-        'form': form,
+        'form_contact': form_contact,
+        'form_person': form_person,
         },
         context_instance=RequestContext(request)
         )
