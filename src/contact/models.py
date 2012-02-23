a3e98b507682657846ff715ed964fbdecd54e391
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-02-23 19:12:49 +0200

    ticket:10 update signals implemented

--- src/contact/models.py
+++ src/contact/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
 
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
@@ -21,3 +24,36 @@ class Contact(models.Model):
 
     def __unicode__(self):
         return u'%s' % (self.email)
+
+
+class Update(models.Model):
+    # Letters from CRUD (no Read)
+    TYPE_CHOICES = (
+            ('C', 'create'),
+            ('U', 'update'),
+            ('D', 'delete'),
+            )
+    update_type = models.CharField(max_length=1, choices=TYPE_CHOICES)
+    model_name = models.CharField(max_length=30)
+    time = models.DateTimeField(auto_now_add=True)
+
+
+@receiver(post_save)
+def update_callback(sender, **kwargs):
+    model_name = sender._meta.object_name
+    if model_name == 'Update':
+        return
+    update_type = 'C' if kwargs.get('created', False) else 'U'
+    print kwargs
+    u = Update(update_type=update_type, model_name=model_name)
+    u.save()
+
+
+@receiver(post_delete)
+def delete_callback(sender, **kwargs):
+    model_name = sender._meta.object_name
+    if model_name == 'Update':
+        return
+    update_type = 'D'
+    u = Update(update_type=update_type, model_name=model_name)
+    u.save()
